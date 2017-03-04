50457c823b380a3a2beeb5f0e2cb197fa788a2e9
Nataniel Paiva <mataniel.paiva@gmail.com>
2017-03-04 12:29:45 -0300

    Subindo alterações

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
     }
 
     /**
